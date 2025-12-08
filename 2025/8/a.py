f94a866003a1ae6042feb7249244a2cbf91d064b
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2025-12-08 08:48:32 +0300

    do 8th day

---
+++ 2025/8/a.py
@@ -0,0 +1,67 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.size = [1] * n
+
+    def find(self, x):
+        while self.par[x] != x:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+
+    def union(self, x, y):
+        par_x = self.find(x)
+        par_y = self.find(y)
+
+        if par_x == par_y:
+            return False
+
+        if self.size[par_x] >= self.size[par_y]:
+            self.par[par_y] = par_x
+            self.size[par_x] += self.size[par_y]
+        else:
+            self.par[par_x] = par_y
+            self.size[par_y] += self.size[par_x]
+
+        return True
+
+
+def get_input():
+    coords = []
+    while True:
+        try:
+            coord = list(map(int, input().strip().split(",")))
+            coords.append(coord)
+        except EOFError:
+            break
+    return coords
+
+
+def get_dist(first, second):
+    x1, y1, z1 = first
+    x2, y2, z2 = second
+
+    return pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2)
+
+
+coordinates = get_input()
+n = len(coordinates)
+
+distances = []
+
+for i in range(n):
+    for j in range(i + 1, n):
+        curr_dist = get_dist(coordinates[i], coordinates[j])
+        distances.append([curr_dist, i, j])
+uf = UnionFind(n)
+distances.sort()
+# limit = 1000
+last = (0, 1)
+for i in range(len(distances)):
+    _, first_ind, second_ind = distances[i]
+    if uf.union(first_ind, second_ind):
+        last = (first_ind, second_ind)
+
+x1, _, _ = coordinates[last[0]]
+x2, _, _ = coordinates[last[1]]
+print(x1 * x2)
